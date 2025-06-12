3278dfcd7d48c0a03a0da0bc07d91ce6a31af264
JoaoFurlanCosta <joaovitorfurlandacosta@hotmail.com>
2025-06-12 11:07:30 -0300

    Exercício palíndromo verificador

---
+++ Exercicios/VerificadorPalindromo/VerificadorPalindromo.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <cctype>
+
+using namespace std;
+
+class VerificadorPalindromo {
+public:
+    VerificadorPalindromo() = default;
+    bool ehPalindromo(const string& texto) {
+        string textoLimpo = limparString(texto);
+        string textoReverso = textoLimpo;
+        reverse(textoReverso.begin(), textoReverso.end());
+        
+        return textoLimpo == textoReverso;
+    }
+
+private:
+    string limparString(const string& str) {
+        string limpa;
+        for (char c : str) {
+            if (isalnum(c)) {
+                limpa += tolower(c);
+            }
+        }
+        return limpa;
+    }
+};
+
+int main() {
+    VerificadorPalindromo verificador;
+
+    string entrada;
+    cout << "\nDigite uma palavra ou frase para verificar se é um palíndromo: ";
+    getline(cin, entrada);
+    if (verificador.ehPalindromo(entrada)) {
+        cout << "'" << entrada << "' é um palíndromo." << endl;
+    } else {
+        cout << "'" << entrada << "' não é um palíndromo." << endl;
+    }
+
+    return 0;
+}
